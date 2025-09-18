cfe02a155287779303be926d7cf464ac72df18f2
Nesta <nestasuo@gmail.com>
2025-09-18 21:21:52 +0200

    Update P0 projekt.py

--- P0 projekt.py
+++ P0 projekt.py
@@ -67,28 +67,46 @@ async def black_line_counter(black):
     if black == 3:
         motor_pair.stop(motor_pair.PAIR_1)
         await runloop.sleep_ms(500)
-        motor_pair.move_for_degrees(motor_pair.PAIR_1,400,-60,velocity=-400)
-        await runloop.sleep_ms(500)
-        motor_pair.move_for_degrees(motor_pair.PAIR_1,500,0,velocity=-400)
-        await runloop.sleep_ms(500)
+        motor_pair.move_for_degrees(motor_pair.PAIR_1,400,-50,velocity=-400)
         motor.run(port.A,200)
+        await runloop.sleep_ms(700)
+        reflectionD = color_sensor.reflection(port.D)
+        while reflectionD > 70:
+            reflectionD = color_sensor.reflection(port.D)
+            motor_pair.move_for_degrees(motor_pair.PAIR_1,300,0,velocity=-300)
+        
         reflectionC = color_sensor.reflection(port.C)
         reflectionD = color_sensor.reflection(port.D)
-        while reflectionC > 30 and reflectionD > 30:
+        while color_sensor.color(port.C) or color_sensor.color(port.D)!= color.BLACK:
+            color_sensor.color(port.C)
+            color_sensor.color(port.D)
             reflectionC = color_sensor.reflection(port.C)
             reflectionD = color_sensor.reflection(port.D)
+            if color_sensor.color(port.C) and color_sensor.color(port.D) is color.BLUE:
+                motor_pair.move_for_degrees(motor_pair.PAIR_1,500,0,velocity=-500)
+                await runloop.sleep_ms(1000)
+                break
             if 30 < reflectionD < 70 and 30 < reflectionC < 70:
-                motor_pair.move(motor_pair.PAIR_1,0,velocity=-650)
+                motor_pair.move(motor_pair.PAIR_1,0,velocity=-450)
             elif 30 < reflectionD < 70:
-                motor_pair.move(motor_pair.PAIR_1,15,velocity=-500)
+                motor_pair.move(motor_pair.PAIR_1,15,velocity=-400)
             elif 30 < reflectionC < 70:
-                motor_pair.move(motor_pair.PAIR_1,-15,velocity=-500)
+                motor_pair.move(motor_pair.PAIR_1,-15,velocity=-400)
         motor_pair.stop(motor_pair.PAIR_1)
-        motor_pair.move_for_degrees(motor_pair.PAIR_1,-1000,0,velocity=-500)
+        motor_pair.move_for_degrees(motor_pair.PAIR_1,-1000,0,velocity=-400)
+        await runloop.sleep_ms(1500)
+        motor.run(port.A,-200)
+        motor_pair.move_for_degrees(motor_pair.PAIR_1,400,90,velocity=-400)
         await runloop.sleep_ms(1000)
-        motor_pair.move_for_degrees(motor_pair.PAIR_1,400,-100,velocity=-500)
-        await runloop.sleep_ms(1000)
-        return
+        reflectionC = color_sensor.reflection(port.C)
+        reflectionD = color_sensor.reflection(port.D)
+
+        while reflectionC > 70 and reflectionD > 70:
+            reflectionC = color_sensor.reflection(port.C)
+            reflectionD = color_sensor.reflection(port.D)
+            motor_pair.move_for_degrees(motor_pair.PAIR_1,200,0,velocity=-600)
+        else:
+            return
     if black == 4:
         return
 
