dc928454000bfec2fbf411e65aad41416f16cf8d
Nesta <nestasuo@gmail.com>
2025-09-18 12:35:20 +0200

    Update P0 projekt.py

--- P0 projekt.py
+++ P0 projekt.py
@@ -29,64 +29,65 @@ async def main():
     while go:
         reflectionC = color_sensor.reflection(port.C)
         reflectionD = color_sensor.reflection(port.D)
-        if 30 < reflectionD < 80 and 30 < reflectionC < 80:
-            motor_pair.move(motor_pair.PAIR_1,0,velocity=300)
-        elif 30 < reflectionD < 80:
-            motor.run(port.E,-200)
-            motor.run(port.F,100)
-        elif 30 < reflectionC < 80:
-            motor.run(port.E,-100)
-            motor.run(port.F,200)
+        if 30 < reflectionD < 70 and 30 < reflectionC < 70:
+            motor_pair.move(motor_pair.PAIR_1,0,velocity=-400)
+        elif 30 < reflectionD < 70:
+            motor.run(port.E,200)
+            motor.run(port.F,-100)
+        elif 30 < reflectionC < 70:
+            motor.run(port.E,100)
+            motor.run(port.F,-200)
         elif 30 > reflectionD or 30 > reflectionC:
             black += 1
             await black_line_counter(black)
 
 async def black_line_counter(black):
     if black == 1:
-        motor_pair.move(motor_pair.PAIR_1,0,velocity=300)
-        time.sleep(0.4)
+        motor_pair.move(motor_pair.PAIR_1,0,velocity=-1050)
+        time.sleep(0.2)
+        motor_pair.stop(motor_pair.PAIR_1)
         reflectionC = color_sensor.reflection(port.C)
         reflectionD = color_sensor.reflection(port.D)
-        while 80 < reflectionD and 80 < reflectionC:
+        while 70 < reflectionD and 70 < reflectionC:
             reflectionC = color_sensor.reflection(port.C)
             reflectionD = color_sensor.reflection(port.D)
-            motor.run(port.E,-250)
-            motor.run(port.F,150)
-            reflectionD = color_sensor.reflection(port.D)
+            motor.run(port.E,150)
+            motor.run(port.F,-250)
         else:
-            motor_pair.stop(motor_pair.PAIR_1)
-            motor.stop(port.E)
-            motor.run(port.F,250)
-            time.sleep(0.9)
             return
     if black == 2:
-        motor_pair.move(motor_pair.PAIR_1,0,velocity=300)
-        time.sleep(0.4)
+        motor_pair.move(motor_pair.PAIR_1,0,velocity=-1050)
+        time.sleep(0.2)
         reflectionC = color_sensor.reflection(port.C)
         reflectionD = color_sensor.reflection(port.D)
-        while 80 < reflectionD and 80 < reflectionC:
+        while 70 < reflectionD and 70 < reflectionC:
             reflectionC = color_sensor.reflection(port.C)
             reflectionD = color_sensor.reflection(port.D)
-            motor.run(port.E,-150)
-            motor.run(port.F,250)
+            motor.run(port.E,350)
+            motor.run(port.F,-300)
             reflectionD = color_sensor.reflection(port.D)
         else:
-            motor_pair.stop(motor_pair.PAIR_1)
-            motor.stop(port.F)
-            motor.run(port.E,-250)
-            time.sleep(0.9)
             return
     if black == 3:
         reflectionC = color_sensor.reflection(port.C)
         reflectionD = color_sensor.reflection(port.D)
-        motor_pair.move(motor_pair.PAIR_1)
+        runloop.run(klo())
+        motor_pair.move_for_degrees(motor_pair.PAIR_1,200,-30,velocity=-500)
+        time.sleep(0.5)
+        motor_pair.move_for_degrees(motor_pair.PAIR_1,200,0,velocity=-500)
+        time.sleep(0.5)
+        while True:
+            reflectionC = color_sensor.reflection(port.C)
+            reflectionD = color_sensor.reflection(port.D)
+            if 30 < reflectionD < 70 and 30 < reflectionC < 70:
+                motor_pair.move(motor_pair.PAIR_1,0,velocity=-400)
+            elif 30 < reflectionD < 70:
+                motor.run(port.E,200)
+                motor.run(port.F,-100)
+            elif 30 < reflectionC < 70:
+                motor.run(port.E,100)
+                motor.run(port.F,-200)
 
-runloop.run(main())
-
-
-
-
-"""
 async def klo():
     while True:
         afstand = distance_sensor.distance(port.B)
@@ -100,11 +101,10 @@ async def klo():
 
 runloop.run(klo())
 runloop.run(main())
-"""
 
 
 
-#FUCK HAN ER COOKED
+#huge fuck up
 from hub import light_matrix
 from hub import port
 import runloop
@@ -114,28 +114,39 @@ import color_sensor
 from hub import port
 import color
 import time
+import distance_sensor
 
+"""
+Port E og F er de 2 små motorer
+Port A er den store motor
+Port C og D er farvesensorer (C er den venstre farvesensor)
+Port B er afstandssensor
+"""
 motor_pair.pair(motor_pair.PAIR_1, port.E, port.F)
 
-motor_pair.move_for_degrees(motor_pair.PAIR_1,-200,100,velocity=1050)
-time.sleep(0.7)
-motor_pair.move_for_degrees(motor_pair.PAIR_1,300,0,velocity=-1050)
-time.sleep(0.7)
+async def pis():
+        reflectionC = color_sensor.reflection(port.C)
+        reflectionD = color_sensor.reflection(port.D)
 
-while True:
-    reflectionC = color_sensor.reflection(port.C)
-    reflectionD = color_sensor.reflection(port.D)
-    if 30 < reflectionD < 80 and 30 < reflectionC < 80:
-        motor_pair.move(motor_pair.PAIR_1,0,velocity=-300)
-    elif 30 < reflectionD < 80:
-        motor.run(port.E,200)
-        motor.run(port.F,-100)
-    elif 30 < reflectionC < 80:
-        motor.run(port.E,100)
-        motor.run(port.F,-200)
-    elif color_sensor.color(port.C) or color_sensor.color(port.D) is color.BLUE:
-        motor_pair.stop(motor_pair.PAIR_1)
-        break
-time.sleep(1)
-motor_pair.move_for_degrees(motor_pair.PAIR_1,100,0,velocity=1050)
+        motor_pair.move_for_degrees(motor_pair.PAIR_1,550,0,velocity=-500)
+        time.sleep(1)
+        motor_pair.move_for_degrees(motor_pair.PAIR_1,160,-100,velocity=-500)
+        time.sleep(1)
+        while True:
+            reflectionC = color_sensor.reflection(port.C)
+            reflectionD = color_sensor.reflection(port.D)
+            afstand = distance_sensor.distance(port.B)
+            if 30 < reflectionD < 70 and 30 < reflectionC < 70:
+                motor_pair.move(motor_pair.PAIR_1,0,velocity=-400)
+            elif afstand is not -1 and afstand <= 170:
+                motor.run(port.A, -200)
+            elif 30 < reflectionD < 70:
+                motor.run(port.A, 100)
+                motor.run(port.E,200)
+                motor.run(port.F,-100)
+            elif 30 < reflectionC < 70:
+                motor.run(port.A, 100)
+                motor.run(port.E,100)
+                motor.run(port.F,-200)
 
+runloop.run(pis())
