f60a424cd4786a6fa8744b9d984fd99c96124a78
Nesta <nestasuo@gmail.com>
2025-09-19 12:50:57 +0200

    Update P0 projekt.py

--- P0 projekt.py
+++ P0 projekt.py
@@ -148,18 +148,39 @@ async def black_line_counter(black):
         await runloop.sleep_ms(1000)
 
     if black == 8:
-        motor_pair.move_for_degrees(motor_pair.PAIR_1,2500,0,velocity=-400,acceleration=500)
+        motor_pair.move_for_degrees(motor_pair.PAIR_1,2400,0,velocity=-400,acceleration=500)
         await runloop.sleep_ms(3000)
-        motor_pair.move_for_degrees(motor_pair.PAIR_1,200,70,velocity=-400,acceleration=500)
+        motor_pair.move_for_degrees(motor_pair.PAIR_1,200,50,velocity=-400,acceleration=500)
         await runloop.sleep_ms(1000)
         motor.run(port.A,200)
         motor_pair.move_for_degrees(motor_pair.PAIR_1,1500,0,velocity=-400,acceleration=500)
         await runloop.sleep_ms(2000)
-        motor.run(port.A,-200)
-        if color_sensor.color(port.C) and color_sensor.color(port.D) is color.BLUE:
-            motor_pair.move_for_degrees(motor_pair.PAIR_1,-500,0,velocity=-500)
-            await runloop.sleep_ms(1500)
-            return
+        while True:
+            reflectionC = color_sensor.reflection(port.C)
+            reflectionD = color_sensor.reflection(port.D)
+            motor.run(port.A,-200)
+            if color_sensor.color(port.C) or color_sensor.color(port.D) is color.BLUE:
+                motor_pair.move_for_degrees(motor_pair.PAIR_1,-1100,0,velocity=-500)
+                await runloop.sleep_ms(2500)
+                motor_pair.stop
+                await runloop.sleep_ms(500)
+                motor.run(port.A,200)
+                await runloop.sleep_ms(1000)
+                motor_pair.move_for_degrees(motor_pair.PAIR_1,-500,0,velocity=-500)
+                motor.run(port.A,-200)
+                await runloop.sleep_ms(1000)
+                motor_pair.move_for_degrees(motor_pair.PAIR_1,-1000,100,velocity=-500)
+                await runloop.sleep_ms(1200)
+                motor_pair.move_for_degrees(motor_pair.PAIR_1,1500,0,velocity=-400,acceleration=500)
+                await runloop.sleep_ms(5000)
+                motor_pair.move_for_degrees(motor_pair.PAIR_1,200,100,velocity=-400,acceleration=500)
+                await runloop.sleep_ms(1000)
+                motor_pair.move_for_degrees(motor_pair.PAIR_1,1000,0,velocity=-400,acceleration=500)
+                await runloop.sleep_ms(1000)
+
+
+      
+                return
 
 
         motor_pair.stop(motor_pair.PAIR_1)
